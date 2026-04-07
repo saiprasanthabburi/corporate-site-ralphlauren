88d32d82bc44381ec38b702f5b1bfcd888ab00c6
Nizamuddin <k.nizamuddin@ralphlauren.com>
2026-04-07 16:36:21 +0530

    fix for header mobile js fix11 desktop 7wf with functionlity

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -191,7 +191,7 @@ export default async function decorate(block) {
   const searchIcon = document.createElement('div');
   searchIcon.classList.add('nav-search');
   searchIcon.innerHTML = `<a href="/search" aria-label="Search">
-    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none" stroke="currentColor" stroke-width="1.5">
+    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none" stroke="currentColor" stroke-width="0.5">
       <circle cx="6" cy="6" r="5"/>
       <line x1="9.5" y1="9.5" x2="14" y2="14"/>
     </svg>
